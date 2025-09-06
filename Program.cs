9d79c0f7ea25bb58e16a850252c509e434f686f3
Arsham Nazaretyan <nazaretyanarsham@gmail.com>
2025-09-07 01:34:12 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace PrimeNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.Write("Enter a number: ");
+            string answer = !int.TryParse(Console.ReadLine(), out n) ? "Error" : PrimeNum(n) ? "Your number is prime" : "Your number is composite";
+            Console.WriteLine(answer);
+        }
+        public static bool PrimeNum(int n)
+        {
+            if(n==2) return true;
+            int sqrt=(int)Math.Sqrt(n);
+            for(int i=2;i<=sqrt+1;++i)
+            {
+                if(n%i==0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
